7db939d902c663fd436eb2f0718e2c8739379f3f
David Busby <d.busby@saiweb.co.uk>
2011-06-27 13:18:16 +0100

    AND ENGINE IS NOT NULL; added

--- index_rebuild.py
+++ index_rebuild.py
@@ -54,7 +54,7 @@ def main():
 
                 db = MySQLdb.connect(host="localhost",user=usr,passwd=pwd,db="information_schema")
                 cursor = db.cursor()
-                sql="SELECT CONCAT(TABLE_SCHEMA,'.',TABLE_NAME) AS TABLE_NAME FROM TABLES WHERE TABLE_SCHEMA NOT IN ('information_schema','mysql','test');"
+                sql="SELECT CONCAT(TABLE_SCHEMA,'.',TABLE_NAME) AS TABLE_NAME FROM TABLES WHERE TABLE_SCHEMA NOT IN ('information_schema','mysql','test') AND ENGINE IS NOT NULL;"
                 cursor.execute(sql)
                 res = cursor.fetchall()
 		i = 0
