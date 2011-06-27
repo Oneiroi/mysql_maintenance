c469bd5992169694549de38442077d1cafb48f1e
David Busby <Buzz@David-Busbys-MacBook-Pro.local>
2011-06-27 12:52:11 +0100

    Index rebuild alpha code

---
+++ index_rebuild.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import os,sys,ConfigParser,getopt
+import MySQLdb
+from time import time,strftime,gmtime
+
+'''
+This utility is designed to be run as a crojob, it will run 'analyze table' on every table to force  a rebuild of indexes, ensure therefor that this is not done at peak times for your web app.
+__author__="David Busby"
+__copyright__="David Busby Saiweb.co.uk"
+__license__="GNU v3 + part 5d section 7: Redistribution/Reuse of this code is permitted under the GNU v3 license, as an additional term ALL code must carry the original Author(s) credit in comment form."
+__version__="0.2"
+'''
+
+def log(log,str):
+    str = '[%s] %s\n' %((strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())),str)
+    f = file(log, 'a+')
+    f.write(str)
+    f.close()
+
+def usage():
+	print 'Usage:',sys.argv[0],'-c /root/.my.cnf'
+	print 'Note: -c is an optional overide, assumes default location if missing'
+	sys.exit(0)
+
+def main():
+	try:
+		opts,args = getopt.getopt(sys.argv[1:],"hc:",["help","config="])
+	except getopt.GetoptError,err:
+		print str(err)
+		usage()
+	logfile = '/var/log/mysql_index_rebuild.log'
+	cnf = '/root/.my.cnf'
+	for o, a in opts:
+		if o in ('-c','--config'):
+			cnf = a
+		else:
+			assert False,"Unsupported option"
+
+        if not os.path.isfile(cnf):
+                print 'Configuration file does not exist!',cnf
+                sys.exit(1)
+        else:
+                cfg = ConfigParser.ConfigParser()
+                cfg.read(cnf)
+
+		try:
+                	usr = cfg.get('client','user')
+                	pwd = cfg.get('client','password')
+		except (ConfigParser.NoOptionError or ConfigParser.NoSectionError), e:
+			print 'Missing a required configuration option'
+                	print e
+                	sys.exit(1)
+
+                db = MySQLdb.connect(host="localhost",user=usr,passwd=pwd,db="information_schema")
+                cursor = db.cursor()
+                sql="SELECT CONCAT(TABLE_SCHEMA,'.',TABLE_NAME) AS TABLE_NAME FROM TABLES;"
+                cursor.execute(sql)
+                res = cursor.fetchall()
+		i = 0
+                for row in res:
+                        sql = 'analyze table %s' % row[0]
+                        log(logfile,'Analyzing table %s' % (row[0])
+                        cursor.execute(sql)
+			ares = cursore.fetchall()
+			log(logfile,'Analyze complete for %s returned %s %s' %(row[0],ares[0][0],ares[0][1])
+if __name__ == '__main__':
+        main()
+
